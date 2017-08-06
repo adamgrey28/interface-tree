0c7ab42dcea1f7e25ba6200f9ccfb44c6bbb87d8
Adam Grey <adamgrey28@gmail.com>
2017-08-06 21:46:43 +0300

    Works!

--- main.js
+++ main.js
@@ -67,92 +67,18 @@ const tree = {
   ]
 };
 
-const tree2 = {
-  'pizza': {
-    content: 'Пицца',
-    children: ['openPizza', 'closedPizza'],
-    parent: null
-  },
-
-  'openPizza': {
-    content: 'Открытая пицца',
-    children: ['openPizza 1', 'openPizza 2'],
-    parent: 'pizza'
-  },
-
-  'closedPizza': {
-    content: 'Закрытая пицца',
-    children: ['closedPizza 1', 'closedPizza 2'],
-    parent: 'pizza'
-  },
-
-  'openPizza 1': {
-    content: 'Открытая пицца 1',
-    children: null,
-    parent: 'openPizza'
-  },
-
-  'openPizza 2': {
-    content: 'Открытая пицца 2',
-    children: null,
-    parent: 'openPizza'
-  },
-
-  'drinks': {
-    content: 'Напитки',
-    children: ['tea', 'coke'],
-    parent: null
-  },
-
-  'coke': {
-    content: 'Кола',
-    children: ['dietCoke', 'regularCoke'],
-    parent: 'drinks'
-  },
-
-  'dietCoke': {
-    content: 'Кола',
-    children: null,
-    parent: 'coke'
-  },
-
-  'regularCoke': {
-    content: 'Кола',
-    children: null,
-    parent: 'coke'
-  },
-
-  'tea': {
-    content: 'Чай',
-    children: ['iceTea', 'blackTea'],
-    parent: 'drinks'
-  },
-
-  'icetea': {
-    content: 'Холодный чай',
-    children: null,
-    parent: 'tea'
-  },
-
-  'blackTea': {
-    content: 'Черный чай',
-    children: null,
-    parent: 'tea'
-  }
-};
 
 const vm = new Vue({
   el: "#app",
   data: {
     /*data from server*/
-    tree: tree,
+    dataFromServer: tree,
     /*conditional render with different styles */
     state: 0,
     /*curr leaf, render its child*/
     currItem: tree,
     /*for go back */
-    lastIndex: -1,
-    prevItem: null
+    path: []
   },
 
   methods: {
@@ -162,7 +88,8 @@ const vm = new Vue({
      */
     goFurther(index) {
       if (this.currItem.children[index].children !== null) {
-        this.prevItem = this.currItem;
+        //this.prevItem = this.currItem;
+        this.path.push(this.currItem);
         this.currItem = this.currItem.children[index];
         if (this.currItem.children[index].children === null) {
           this.state = 1;
@@ -171,7 +98,11 @@ const vm = new Vue({
     },
 
      goBack() {
-       this.currItem = this.prevItem;
+       this.currItem = this.path[this.path.length - 1];
+       this.path.splice(this.path.length - 1, 1);
+       if (this.state === 1) {
+         this.state = 0;
+       };
     }
   }
 });
